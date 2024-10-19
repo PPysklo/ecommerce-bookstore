dbee745d4c9de3029e921de47f6a8e6e81a87cdb
PPysklo <125765791+PPysklo@users.noreply.github.com>
2024-10-19 17:01:23 +0200

    Added navbar and some other changes (#1)

---
+++ static/js/navbar_scripts.js
@@ -0,0 +1,15 @@
+// Obsługuje rozwijanie i zwijanie dropdown menu
+$(document).ready(function() {
+    $('.dropdown-toggle').on('click', function() {
+        $(this).parent().toggleClass('show');
+        $(this).next('.dropdown-menu').toggleClass('show');
+    });
+
+    // Zamknij dropdown po kliknięciu poza nim
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('.dropdown').length) {
+            $('.dropdown-menu').removeClass('show');
+            $('.dropdown').removeClass('show');
+        }
+    });
+});
