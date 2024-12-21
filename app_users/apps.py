9465131e19af6f466b903fa1a80ae3de7db9f213
PPysklo <ppysklo01@gmail.com>
2024-12-21 23:40:45 +0100

    Add README file, update .gitignore, and implement order management permissions for Order manager group

--- app_users/apps.py
+++ app_users/apps.py
@@ -1,5 +1,6 @@
+# filepath: /c:/Users/Piotr/Desktop/Python/Inzynierka/bookstore/app_users/apps.py
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class AppUsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -7,4 +8,29 @@ class AppUsersConfig(AppConfig):
     verbose_name = "Użytkownicy"
 
     def ready(self):
-        import app_users.signals
+        post_migrate.connect(self.create_order_manager_group, sender=self)
+
+    @staticmethod
+    def create_order_manager_group(sender, **kwargs):
+        from django.apps import apps
+        Group = apps.get_model('auth', 'Group')
+        Permission = apps.get_model('auth', 'Permission')
+
+        # Tworzenie grupy
+        order_manager_group, created = Group.objects.get_or_create(name='Order Manager')
+        
+        # Przypisywanie uprawnień
+        permissions = Permission.objects.filter(codename__in=[
+            'can_change_order',
+            'can_delete_order',
+            'can_view_order',
+            'can_change_order_item',
+            'can_delete_order_item',
+            'can_view_order_item',
+            'can_add_shipping_address',
+            'can_change_shipping_address',
+            'can_view_shipping_address',
+        ])
+        
+        order_manager_group.permissions.set(permissions)
+        order_manager_group.save()
